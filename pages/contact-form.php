ee03f4fcfa9eb99342b61297f8848638d9620d4c
Mehmed Bosnic <96009692+mbosnic96@users.noreply.github.com>
2023-12-11 16:49:05 +0100

    Update contact-form.php

--- pages/contact-form.php
+++ pages/contact-form.php
@@ -1,10 +1,11 @@
 <div class="col-md-12">
-    <form action="send_email.php" method="post">
+    <form action="pages/send_email.php" method="post">
     <input type="text" class="form-control mt-4" id="name" name="name" placeholder="Naam" required="true">
     <input type="tel" class="form-control mt-4" id="phone" name="name" placeholder="Telefoonnummer" required="true">
     <input type="email" class="form-control mt-4" id="email" name="email" placeholder="Email" required="true">
     <textarea class="form-control mt-4" id="message" rows="3" name="message" placeholder="Waar kunnen we je mee helpen?" required="true"></textarea>
+   
+    <button type="submit" class="btn btn-danger mb-4 btn-lg mt-4">Verzend</button>
     </form>
 
-    <button type="submit" class="btn btn-danger mb-4 btn-lg mt-4">Verzend</button>
 </div>
